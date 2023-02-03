ac8c3ed552eb0b6acd8b4e86f778bd871c69ae85
Kieren Hetherington <98293613+KHetherington89@users.noreply.github.com>
2023-02-03 09:18:14 +0000

    Create html and js files required.
    index.html and rps.js created. index.html boilerplate, script link
    and instructional headers added. Scope laid out in rps.js

---
+++ rps.js
@@ -0,0 +1,20 @@
+// Rock Paper Scissors
+
+// PROBLEM: Create a version of rock paper scissors that can be played in the developer console
+
+// BREAKDOWN:
+
+// 1 - Define values for each option (rock, paper, scissors)
+// 2 - Create variables to store players choice and computers choice
+// 3 - Make computer choose between the options at random
+// 4 - Instruct and prompt player
+// 5 - Allow player to input their choice
+// 6 - Check that player input is valid
+// 7 - Compare choices to decide winner
+// 8 - Declare winner
+// 9 - Track scores for each player
+// 10 - Best of 5
+//      - Count number of rounds played
+//      - If rounds < 5, play again
+//      - If rounds >= 5, stop
+//      - Determine highest score and declare overall winner
