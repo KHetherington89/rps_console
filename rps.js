8b1035f6fc1fd2bb061f0d5287c2570a32e15a6d
Kieren Hetherington <98293613+KHetherington89@users.noreply.github.com>
2023-02-07 19:28:58 +0000

    Create basic functionality with buttons.
    Computer selection code modified to take the choice from an array
    containing all the options.
    Player input tied to event listeners on the 3 buttons added to the
    page.
    When the player clicks the game now makes a selection for the computer,
    compares it to the player choice, and outputs the choices and round
    result to the console.

--- rps.js
+++ rps.js
@@ -2,87 +2,36 @@
 
 // VALUES: 1=Rock, 2=Paper, 3=Scissors
 
-// Generates a random number 1-3
+let weapons = ["rock", "paper", "scissors"];
+
+const btns = document.querySelectorAll('.btn');
+    btns.forEach(btn => {
+        btn.addEventListener('click', playerClicks) 
+    });
+
 function getCompChoice(){
-    let compChoice;
-    compChoice = Math.floor(Math.random()*3) + 1
-    console.log("compChoice " + compChoice);
+    let compChoice = weapons.at(Math.floor(Math.random()*(weapons.length)))
+    console.log(compChoice);
     return compChoice;
 }
 
-
-/* Prompts the player for input and immediately converts to lowercase before storing in playChoice
-    The +"a" on the prompt here prevents an error if the player cancels by preventing the prompt returning null,
-    hence the extra a on the options.*/
-function getPlayChoice(){
-    let playChoice
-    playChoice = ("a"+prompt("Choose Your Weapon!","")).toLowerCase();
-    // While loop repeats request for input if an invalid choice is made
-    while (playChoice != "arock" && playChoice != "apaper" && playChoice != "ascissors"){
-        playChoice = ("a"+prompt("Please make a valid choice!","")).toLowerCase();
+function playerClicks(){
+    playChoice=(this.id);
+    console.log(playChoice);
+    let compChoice = getCompChoice();
+    if (compChoice === playChoice){
+        console.log("tie");
     }
-    console.log("playChoice " + playChoice);
-    // Converts playChoice to numbers, just for easier comparison to compChoice
-    if (playChoice == "arock"){
-        playChoice = 1;
-    }
-    else if (playChoice == "apaper"){
-        playChoice = 2;
-    }
-    else if (playChoice == "ascissors"){
-        playChoice = 3;
-    }
-    console.log("playChoice " + playChoice);
-    return playChoice;
+    else if((playChoice === "rock" && compChoice === "paper") || (playChoice === "paper" && compChoice === "scissors") ||
+            (playChoice === "scissors" && compChoice === "rock")){
+                console.log("You lose");
+            }
+    else if((compChoice === "rock" && playChoice === "paper") || (compChoice === "paper" && playChoice === "scissors") ||
+    (compChoice === "scissors" && playChoice === "rock")){
+        console.log("You win");
+    } 
 }
 
-
-// Compares player inputs, determines winner, adds to scores, and alerts player of outcome
-function playRound (playChoice, compChoice){
-    let winner;
-    // 1st IF determines tie game
-    if (playChoice == compChoice) { winner = "tie" }
-    // 2nd IF determines player win
-    else if (playChoice > compChoice ||((compChoice == 3)&&(playChoice == 1))) { winner = "player" }
-    // 3rd IF determines computer win
-    else if (compChoice > playChoice ||((playChoice == 3)&&(compChoice == 1))) { winner = "comp" }
-    return winner;
-}
-
-function playGame(){
-    alert("Welcome to Rock Paper Scissors \n First to 5 \n Just type your choice in the prompt!");
-    let playScore = 0;
-    let compScore = 0;
-    // While loop looks for either players score to reach 5
-    while((playScore < 5) && (compScore < 5)){
-        let winner = playRound(getPlayChoice(),getCompChoice())
-        // Tie game doesn't increment any score counter
-        if (winner == "tie"){
-        alert("Tie game! Try again");
-        }
-        // Increments the player score and announces win
-        else if (winner == "player"){
-            playScore ++
-            alert(`You win this round.\n Player: ${playScore}\n Computer: ${compScore}`);
-        }
-        // Increments computer score and announces win
-        else if (winner = "comp"){
-            compScore ++
-            alert(`Computer wins this round.\n Player: ${playScore}\n Computer: ${compScore}`);
-        }
-    }
-
-    let scoreLine = `Player ${playScore} : ${compScore} Computer`
-
-    if (playScore == 5){
-        alert(`Congratulations, you win. \n ${scoreLine}`)}
-    else 
-    alert(`You lose. Try again. \n ${scoreLine}`);
-    
-    alert("Thank you for playing. \n Reload the page to play again.")
-}
-
-playGame();
 
 
 
