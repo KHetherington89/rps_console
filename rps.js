7b9d5bcaaf2f7a635ea85c1f343551b4023fc46f
Kieren Hetherington <98293613+KHetherington89@users.noreply.github.com>
2023-02-14 18:07:44 +0000

    Add end game overlay. Add more descriptive text.
    div added which gains an "active" class when one side reaches five points
    This div prevents further play, and contains a button which when clicked
    resets the scores and removes the active class. The active class sets the
    fixed position div to cover the full screen.
    
    The descriptive text additions add to the win/lose text on each round, by
    describing how the victor wins. These values were added to the "weapons"
    arrays at the index above the winning weapon they corresond to. To facilitate
    this a function was added which finds the corresponding array to the
    players choice

--- rps.js
+++ rps.js
@@ -1,20 +1,25 @@
 // Remember that compChoice is always given as the name of a const containing an array.
 // playChoice is always taken as a string.
 
-const rock = ["rock", "paper", "spock"];
-const paper = ["paper", "scissors", "lizard"];
-const scissors = ["scissors", "rock", "spock"];
-const lizard = ["lizard", "scissors", "rock"];
-const spock = ["spock", "paper", "lizard"];
+const rock = ["rock", "paper", "covers", "spock", "vaporises"]; // Each "weapon" array has its own name first, followed by pairs of "what beats it" & "how thing beats it"
+const paper = ["paper", "scissors", "cuts", "lizard", "eats"];
+const scissors = ["scissors", "rock", "crushes", "spock", "smashes"];
+const lizard = ["lizard", "scissors", "decapitates", "rock", "crushes"];
+const spock = ["spock", "paper", "disproves", "lizard", "poisons"];
 const weapons = [rock, paper, scissors, lizard, spock];
 const playScoreOut = document.getElementById("playScoreOut");
 const compScoreOut = document.getElementById("compScoreOut");
 const roundText = document.getElementById("roundText");
+const winLoseText = document.getElementById("winLoseText");
+const gameOver = document.getElementById("gameOver");
+const btns = document.querySelectorAll(".btn");
+document.getElementById("newGameButton").addEventListener("click", () => newGame());
 let playScore = 0;
 let compScore = 0;
 
 function getCompChoice(){
     let compChoice = weapons.at(Math.floor(Math.random()*(weapons.length)))
+/*     console.log(compChoice); */
     return compChoice;
     }
 
@@ -28,45 +33,105 @@ function findWinner(playChoice, compChoice){
     return winner;
     }
 
+function firstCaps(stringIn){
+    let stringOut = stringIn.charAt(0).toUpperCase() + stringIn.slice(1);
+    return stringOut;
+}
+// Finds the array that corresponds to the player choice string input. Needed to make the "method of beating" part of the text work properly
+function playChoiceArray(playChoice){
+    let playChoiceArrayInt;
+    let i=0;
+    while (i < weapons.length){
+        if(weapons[i][0] === playChoice){
+            playChoiceArrayInt = weapons[i];
+            break;
+        }
+        i += 1;
+    }
+    return playChoiceArrayInt;
+}
+
+function howItWins(winner, compChoice, playChoice){
+    let index;
+    let howItWins;
+    if(winner == "player"){
+        index = compChoice.indexOf(playChoice);
+        index += 1;
+        howItWins = compChoice[index];
+    }
+    else if(winner == "computer"){
+        let playChoiceArrayExt = playChoiceArray(playChoice);
+        index = playChoiceArrayExt.indexOf(compChoice[0]);
+        console.log(index);
+        console.log(playChoiceArrayExt);
+        index += 1;
+        howItWins = playChoiceArrayExt[index];
+    }
+    return howItWins;
+}
+
+
+
 function textMaker(winner, compChoice, playChoice){
     if (winner == "tie"){
         resultText=("Tie round.");
         }
         else if(winner == "computer"){
-            resultText = (`You lose. ${compChoice[0].charAt(0).toUpperCase() + compChoice[0].slice(1)} beats ${playChoice}`);
+            if (playChoice == "spock"){  //This just capitalises Spock if it won't be capitalised by winning.
+                resultText = (`You lose. ${firstCaps(compChoice[0])} ${howItWins(winner, compChoice, playChoice)} ${firstCaps(playChoice)}`);
+            }
+            else{
+                resultText = (`You lose. ${firstCaps(compChoice[0])} ${howItWins(winner, compChoice, playChoice)} ${playChoice}`);
+            }
         }            
         else if(winner == "player"){
-            resultText = (`You win. ${playChoice.charAt(0).toUpperCase() + playChoice.slice(1)} beats ${compChoice[0]}`);
+            if (compChoice == spock){
+                resultText = (`You win. ${firstCaps(playChoice)} ${howItWins(winner, compChoice, playChoice)} ${firstCaps(compChoice[0])}`);
+            }
+            else{
+                resultText = (`You win. ${firstCaps(playChoice)} ${howItWins(winner, compChoice, playChoice)} ${compChoice[0]}`);
+            }
         }
         return resultText;
     }
     
 function playRound(playChoice){
+    /* console.log(typeof(playChoice)); */
+    let resultText;
     let compChoice = getCompChoice();
     let winner = findWinner(playChoice, compChoice); 
     if (winner == "computer"){compScore ++}
     else if (winner == "player"){playScore ++}
-    let textToShow = textMaker(winner, compChoice, playChoice);
+    resultText = textMaker(winner, compChoice, playChoice);
     playScoreOut.textContent = `${playScore}`;
     compScoreOut.textContent = `${compScore}`;
-    roundText.textContent = `${resultText}`;   
+    roundText.textContent = `${resultText}`;
+    firstToFive(playScore, compScore);
+}
+
+function firstToFive(playScore, compScore){
+    if ((playScore == 5) || (compScore == 5)){
+        gameOver.classList.add("active");
     }
+    if (playScore == 5){winLoseText.textContent = "YOU WIN!"}
+    else {winLoseText.textContent = "YOU LOSE!"}
+}
+    
 
-    const btns = document.querySelectorAll('.btn');
+function addListener(){   
     btns.forEach(btn => {
-        btn.addEventListener('click', function(){
-            playRound(btn.id);
-        }) 
-    });
+        btn.addEventListener("click", () => playRound(btn.id)) 
+    })
+}
+
+function newGame(){
+    gameOver.classList.remove("active");
+    playScore = 0;
+    compScore = 0;
+    playScoreOut.textContent = `${playScore}`;
+    compScoreOut.textContent = `${compScore}`;
+    roundText.textContent = "Begin next round";
+}
 
 
-
-/*     const startText = document.getElementById("startText");
-    startText.textContent = textToShow;
-    const scoreLine = document.createElement("p");
-    scoreLine.textContent = `${playChoice} ${playScore} : ${compScore} ${compChoice[0]}`;
-    const scoreBox = document.getElementById("scoreBox");
-    scoreBox.appendChild(scoreLine);
- */
-
-
+addListener();
