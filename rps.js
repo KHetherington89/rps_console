fc96e3b1d265f42159cbbe9890bbf09369737837
Kieren Hetherington <98293613+KHetherington89@users.noreply.github.com>
2023-02-16 20:20:30 +0000

    Remove needless extra declaration line.
    Removed let resultText from the playRound function and let it at the point
    where a value is assigned.

--- rps.js
+++ rps.js
@@ -19,13 +19,12 @@ let playScore = 0;
 let compScore = 0;
 
 function playRound(playChoiceString){
-    let resultText;
     let playChoice = playChoiceArrayFind(playChoiceString) //  Immediately "swap" string for corresponding array. Not absolutely required, but makes comparing with comp choice like for like, so less
     let compChoice = getCompChoice();                  //     working out what is required for any given comparison.
     let winner = findWinner(playChoice, compChoice); 
     if (winner == "computer"){compScore ++}
     else if (winner == "player"){playScore ++}
-    resultText = textMaker(winner, compChoice, playChoice);
+    let resultText = textMaker(winner, compChoice, playChoice);
     playScoreOut.textContent = `${playScore}`;
     compScoreOut.textContent = `${compScore}`;
     roundText.textContent = `${resultText}`;
